07f90727fcee9fccf3c4f606265fbad70060a325
repri5e <mikhailp2003@mail.ru>
2020-08-25 16:14:04 +0300

    Minor updates

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const express = require('express')
-const path = require('path')
-const bodyParser = require('body-parser')
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 
-const app = express()
+const app = express();
 const port = 6060;
 const urlencodedParser = bodyParser.urlencoded({extended: false});
+const mongoClient = new MongoClient("mongodb://localhost:27017/", { useNewUrlParser: true });
 
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, "views"));
@@ -22,14 +24,29 @@ app.get('/register', (request, response) => {
 	});
 });
 
+app.post('/register', urlencodedParser, (request, response) => {
+		if (!request.body){
+		redirect('/register');
+	} else {
+		let login = request.body.login_field;
+		let password = request.body.password_field;
+		mongoClient.connect(function(err, client){
+ 
+    	if(err){
+        	return console.log(err);
+    	} else {
+    		console.log('connected to db');
+    		console.log(login, password);
+    		client.close();
+    	}
+});
+	}
+});
+
 app.get('/main', (request, response) => {
     response.render('index', {
         title: 'Главная',     
     });
-});
-
-app.post('/validate', (request, response) => {
-	response.send(' ');
 });
 
 app.listen(port, () => {
